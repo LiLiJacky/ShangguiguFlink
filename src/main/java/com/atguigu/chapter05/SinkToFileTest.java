15596c81b34bd4a55728ba9f6118c51a9fbff2d5
LiLiJacky <ouyangwuchi@icloud.com>
2022-10-23 11:59:50 +0800

    day1

--- src/main/java/com/atguigu/chapter05/SinkToFileTest.java
+++ src/main/java/com/atguigu/chapter05/SinkToFileTest.java
@@ -1,12 +1,44 @@
 package com.atguigu.chapter05;
 
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+
+import java.util.concurrent.TimeUnit;
 
 public class SinkToFileTest {
     public static void main(String[] args) throws Exception{
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
+        env.setParallelism(4);
 
+
+        //  从元素读取数据
+        DataStreamSource<Event> stream = env.fromElements(
+                new Event("mary", "./home", 1000L),
+                new Event("Alice", "./prob?id=100", 2000L),
+                new Event("Bob", "./cart", 3000L),
+                new Event("Bob", "./prob?id=1", 4000L),
+                new Event("Bob", "./home", 4500L)
+        );
+
+        StreamingFileSink<String> streamFileSink = StreamingFileSink.<String>forRowFormat(new Path("./output"),
+                        new SimpleStringEncoder<>("UTF-8"))
+                .withRollingPolicy(
+                        DefaultRollingPolicy.builder()
+                                .withMaxPartSize(1024 * 1024 * 1024)
+                                .withRolloverInterval(TimeUnit.MINUTES.toMillis(15))
+                                .withInactivityInterval(TimeUnit.MINUTES.toMillis(5))
+                                .build()
+                )
+                .build();
+
+        stream.map(data -> data.toString())
+                .addSink(streamFileSink);
+
+        env.execute();
 
     }
 }
